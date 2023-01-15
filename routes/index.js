4f4990a6720478db1130cd65d8d23e8966f17ec5
Jiaqi <dxq7nz@virginia.edu>
2023-01-15 04:03:34 -0500

    Done!

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const Book = require("../models/book");
 
 router.get("/", async (req, res) => {
-  let books = [];
+  let books;
   try {
     books = await Book.find().sort({ createdAt: "desc" }).limit(10).exec();
-  } catch (error) {
+  } catch {
     books = [];
   }
   res.render("index", { books: books });
